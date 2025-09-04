f0d005e60d60c25881a5fa8d6e3320b6ba059300
Armanul Momin <armanul.momin@gmail.com>
2025-09-04 12:27:43 +1000

    Coin Row View v1

---
+++ CryptoSwiftUI/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,21 @@
+//
+//  CoinRowView.swift
+//  CryptoSwiftUI
+//
+//  Created by Arman on 4/9/25.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+    var body: some View {
+        HStack{
+            
+        }
+    }
+}
+
+#Preview {
+    CoinRowView(coin: Preview.dev.coin)
+}
